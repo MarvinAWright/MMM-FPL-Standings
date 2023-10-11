6ab0050bc3ac3ed9fc04d204e1fb157cafcecf53
Marvin Wright <mwright@travix.om>
2023-10-11 20:15:18 +0100

    switching http request dependency from request to got

--- node_helper.js
+++ node_helper.js
@@ -11,46 +11,24 @@ module.exports = NodeHelper.create({
     },
 
 
-    getFplStandingsData: function (payload) {
+    getFplStandingsData: function (url) {
         let that = this;
-        this.url = payload;
+        this.url = url;
 
         got.get(this.url, {
-                responseType: 'json'
-            }
-        ).then(response => {
-            that.result = null;
-            if(response.statusCode === 200) {
-                that.result = response;
-            }
-            // We have the response figured out so lets fire off the notification
+            responseType: 'json'
+        }).then(response => {
+            // console.debug('MMM-FPL-Standings - Status code:' + response.statusCode);
+            // console.debug('MMM-FPL-Standings - response:' + response);
+            // console.debug('MMM-FPL-Standings - body:' + JSON.stringify(response.body));
+            that.result = response.body;
             that.sendSocketNotification('GOT-FPL-STATUS', {'url': that.url, 'result': that.result});
         }).catch(error => {
+            console.error('MMM-FPL-Standings - Status code:' + error.response.statusCode);
             that.result = null;
-            // We have the response figured out so lets fire off the notification
             that.sendSocketNotification('GOT-FPL-STATUS', {'url': that.url, 'result': that.result});
         });
-
-
-
-        // request({url: this.url, method: 'GET'}, function (error, response, body) {
-        //     // Lets convert the body into JSON
-        //     var result = JSON.parse(body);
-        //
-        //     // Check to see if we are error free and got an OK response
-        //     if (!error && response.statusCode === 200) {
-        //         // Let's get the data
-        //         that.result = result;
-        //     } else {
-        //         // In all other cases it's some other error
-        //         that.result = null;
-        //     }
-        //
-        //     // We have the response figured out so lets fire off the notification
-        //     that.sendSocketNotification('GOT-FPL-STATUS', {'url': that.url, 'result': that.result});
-        // });
     },
-
 
     socketNotificationReceived: function (notification, payload) {
         // Check this is for us
